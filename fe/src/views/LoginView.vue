51bc3eb019a763142f9993abc30a3945e90dcfd0
Jack Wickham <jackwickham@live.co.uk>
2022-08-30 22:15:20 +0100

    Fix some FE issues

--- fe/src/views/LoginView.vue
+++ fe/src/views/LoginView.vue
@@ -15,7 +15,9 @@ const error = ref<string | null>(null);
 const auth = getFirebaseAuth();
 const router = useRouter();
 
-const submit = async () => {
+const submit = async (e) => {
+  e.preventDefault();
+
   submitting.value = true;
   try {
     const userCredential = await signInWithEmailAndPassword(auth, credentials.value.email, credentials.value.password);
@@ -40,82 +42,79 @@ onAuthStateChanged(auth, (user) => {
 <template>
   <div class="login">
     <h1>Login</h1>
-    <div class="login-form">
+    <form class="login-form" @submit="submit">
       <p v-if="error !== null" class="login-error">
         {{error}}
       </p>
       <div class="login-input-group">
         <label class="small" for="email">Email</label>
-        <input name="email" type="email" v-model="credentials.email" :disabled="submitting" />
+        <input class="login-input" name="email" type="email" v-model="credentials.email" :disabled="submitting" required="true" />
       </div>
       <div class="login-input-group">
         <label class="small" for="password">Password</label>
-        <input name="password" type="password" v-model="credentials.password" :disabled="submitting" />
+        <input class="login-input" name="password" type="password" v-model="credentials.password" :disabled="submitting" required="true" />
       </div>
-      <button class="login-button" @click="submit" v-if="!submitting">Login</button>
+      <input type="submit" class="login-button" v-if="!submitting" value="Login" />
       <button class="login-button login-button-disabled" @click="submit" v-if="submitting">Logging in...</button>
-    </div>
+    </form>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .login {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 20px;
-  }
+.login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-  .login, .login input {
-    font-size: 1.1rem;
-  }
+.login, .login-input {
+  font-size: 1.1rem;
+}
 
-  .login-form {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 350px;
-  }
+.login-form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 350px;
+  margin-top: 20px;
+}
 
-  .login-error {
-    color: #cc2323;
-    font-size: 0.8em;
-  }
-  
-  .login-input-group label {
-    display: block;
-  }
+.login-error {
+  color: #cc2323;
+  font-size: 0.8em;
+}
 
-  .login input {
-    background-color: #333;
-    color: #ddd;
-    border: 1px solid #555;
-    border-radius: 4px;
-    outline: none;
-    padding: 3px;
-    width: 100%;
-  }
+.login-input-group label {
+  display: block;
+}
 
-  .login input:focus-visible, .login-button:focus-visible {
-    border: 1px solid var( --color-border-focus);
-  }
+.login-input {
+  background-color: #333;
+  color: #ddd;
+  border: 1px solid #555;
+  border-radius: 4px;
+  outline: none;
+  padding: 3px;
+  width: 100%;
+}
 
-  .login-button {
-    padding: 5px;
-    background-color: #333;
-    border: 1px solid hsla(160, 100%, 37%, 0.5);
-    color: #ddd;
-    border-radius: 4px;
-    cursor: pointer;
-    margin-top: 5px;
-    outline: none;
-  }
+.login-input:focus-visible, .login-button:focus-visible {
+  border: 1px solid var( --color-border-focus);
+}
 
-  .login-button-disabled {
-    opacity: 0.7;
-  }
+.login-button {
+  padding: 5px;
+  background-color: #333;
+  border: 1px solid hsla(160, 100%, 37%, 0.5);
+  color: #ddd;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 5px;
+  outline: none;
+}
+
+.login-button-disabled {
+  opacity: 0.7;
 }
 </style>
